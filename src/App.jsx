98bc480fcbe976b34d4a1ce28800916c447eca02
Umaima Zainab <umaimazainab429@gmail.com>
2026-08-29 14:01:01 +0500

    Added loop practice questions

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user="Umaima zainab"  img="https://plus.unsplash.com/premium_photo-1756099779811-baa284e1b864?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4OHx8fGVufDB8fHx8fA%3D%3D" />
-      <Card user=" Zainab fatima" age='29' img="https://images.unsplash.com/photo-1552053831-71594a27632d"/>
-      <Card user="  Aizal khan" age='18' img="https://images.unsplash.com/photo-1679531751641-79f78cbb5c0b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGRpYW1vbmR8ZW58MHx8MHx8fDA%3D"/>
-      <Card user=" Sara faisal" img="https://plus.unsplash.com/premium_photo-1669704098858-8cd103f4ac2e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2FyYXxlbnwwfHwwfHx8MA%3D%3D" />
-      <Card user=" Fatima Khan" img="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8fDA%3D"/>
+      <Card user="Umaima Zainab" img="https://plus.unsplash.com/premium_photo-1756099779811-baa284e1b864?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4OHx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user=" Zainab Fatima" img="https://images.unsplash.com/photo-1552053831-71594a27632d" />
+      <Card user="  Aizal Khan" img="https://images.unsplash.com/photo-1679531751641-79f78cbb5c0b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGRpYW1vbmR8ZW58MHx8MHx8fDA%3D" />
+      <Card user=" Sara Faisal" img="https://plus.unsplash.com/premium_photo-1669704098858-8cd103f4ac2e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2FyYXxlbnwwfHwwfHx8MA%3D%3D" />
+      <Card user=" Fatima Khan" img="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8fDA%3D" />
+      <Card user=" Minal Ali" img="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGh1bWFufGVufDB8fDB8fHww" />
+      <Card user=" Bia Khan" img="https://images.unsplash.com/photo-1595406549310-b878df727a57?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QmlhJTVEfGVufDB8fDB8fHww" />
+      <Card user=" Anfal Fatima" img="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGh1bWFufGVufDB8fDB8fHww" />
+      <Card user=" Dua Ali" img="https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGh1bWFufGVufDB8fDB8fHww" />
+
     </div>
   )
 }
